9b61e8fc04bcb9f6c37343b05fd3a98bf3e12bf7
Scott Dunn <scottrish@mac.com>
2020-09-09 13:48:17 -0400

    Formatting.

--- src/cantina/CLI.java
+++ src/cantina/CLI.java
@@ -14,120 +14,121 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class CLI {
 
-	enum SelectorType {
-		CLASS,
-		CLASSNAMES,
-		IDENTIFIER
+    enum SelectorType {
+	CLASS,
+	CLASSNAMES,
+	IDENTIFIER
+    }
+    
+    public static void main(String[] args) {
+	if (args.length == 0) {
+	    System.out.println("Usage: java cantina.CLI <input filename>");
+	    System.exit(0);
 	}
 	
-	public static void main(String[] args) {
-		if (args.length == 0) {
-			System.out.println("Usage: java cantina.CLI <input filename>");
-			System.exit(0);
-		}
-		
-		InputStream in = null;
-		try {
-			in = new FileInputStream(args[0]);
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found");
-			System.exit(0);
-		}
-		
-		// Use Jackson to construct a tree of JsonNodes
-		ObjectMapper objectMapper = new ObjectMapper();
-		JsonNode jsonNode = null;
-		try {
-			jsonNode = objectMapper.readTree(in);
-		} catch (IOException e) {
-			System.out.println("Unable to process file");
-			System.exit(0);			
-		}
-
-		// As the JsonNode-based tree has a node for each line in the file, it does not reflect
-		// any semantics of the file, so construct a tree of UINodes which does add 
-		// some of those semantics.
-		UINode rootNode = new UINode("root", jsonNode);
-
-		// Accept input from command line and traverse the UINode tree for matches. 
-		while (true) {
-			//Enter data using BufferReader 
-	        BufferedReader reader =  
-	                   new BufferedReader(new InputStreamReader(System.in)); 
-	         
-	        // Reading data using readLine 
-	        String selector = null;
-			try {
-				selector = reader.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} 
-	        if (selector != null && selector.length() > 0) {
-	        	System.out.println("--------------------------------------------------");
-	        	CLI.find(rootNode, selector);
-	        	System.out.println("--------------------------------------------------");
-	        }
-		}
+	InputStream in = null;
+	try {
+	    in = new FileInputStream(args[0]);
+	} catch (FileNotFoundException e) {
+	    System.out.println("File not found");
+	    System.exit(0);
 	}
 	
-	public static void find(UINode node, String selector) {
-		// Determine the selector type to avoid having to parse the selector.
-		// This will need changed to support chained or compound selectors. 
-		SelectorType selType = SelectorType.CLASS;
-		
-		if (selector.charAt(0) == '.') {
-			selector = selector.substring(1);
-			selType = SelectorType.CLASSNAMES;
-		}
-		else if (selector.charAt(0) == '#') {
-			selType = SelectorType.IDENTIFIER;
-			selector = selector.substring(1);
-		}
-		
-		List<UINode> matchingNodes = new ArrayList<UINode>();
-		CLI.find(node, selector, selType, matchingNodes);
-		
-		if (matchingNodes.size() == 0) {
-			System.out.println("No nodes found");
-		}
-		else {
-			System.out.println("Found " + matchingNodes.size() + " nodes:");
-			for (UINode n : matchingNodes) {
-				System.out.println(n);
-				System.out.println();
-			}
-		}
+	// Use Jackson to construct a tree of JsonNodes
+	ObjectMapper objectMapper = new ObjectMapper();
+	JsonNode jsonNode = null;
+	try {
+	    jsonNode = objectMapper.readTree(in);
+	} catch (IOException e) {
+	    System.out.println("Unable to process file");
+	    System.exit(0);			
 	}
 	
-	public static void find(UINode node, String selector, SelectorType selType, List<UINode> matchingNodes) {
-		
-		// Does current node match, if so add to the list
-		switch(selType) {
-			case CLASS: {
-				if (node.getUiClass() != null && node.getUiClass().equals(selector)) {
-					matchingNodes.add(node);
-				}
-				break;
-			}
-			case IDENTIFIER: {
-				if (node.getIdentifier() != null && node.getIdentifier().equals(selector)) {
-					matchingNodes.add(node);
-				}
-				break;
-			}
-			case CLASSNAMES: {
-				if (node.getClassNames().contains(selector)) {
-					matchingNodes.add(node);
-				}
-				break;
-			}
-		}
-
-		// If the node has children continue traversing
-		if (node.getChildren().size() > 0) {
-			for (UINode n : node.getChildren()) {
-				CLI.find(n, selector, selType, matchingNodes);
-			}
-		}
+	// As the JsonNode-based tree has a node for each line in the file, it does not reflect
+	// any semantics of the file, so construct a tree of UINodes which does add 
+	// some of those semantics.
+	UINode rootNode = new UINode("root", jsonNode);
+	
+	// Accept input from command line and traverse the UINode tree for matches. 
+	while (true) {
+	    //Enter data using BufferReader 
+	    BufferedReader reader =  
+		new BufferedReader(new InputStreamReader(System.in)); 
+	    
+	    // Reading data using readLine 
+	    String selector = null;
+	    try {
+		System.out.print("Enter selector: ");
+		selector = reader.readLine();
+	    } catch (IOException e) {
+		e.printStackTrace();
+	    } 
+	    if (selector != null && selector.length() > 0) {
+		System.out.println("--------------------------------------------------");
+		CLI.find(rootNode, selector);
+		System.out.println("--------------------------------------------------");
+	    }
 	}
+    }
+    
+    public static void find(UINode node, String selector) {
+	// Determine the selector type to avoid having to parse the selector.
+	// This will need changed to support chained or compound selectors. 
+	SelectorType selType = SelectorType.CLASS;
+	
+	if (selector.charAt(0) == '.') {
+	    selector = selector.substring(1);
+	    selType = SelectorType.CLASSNAMES;
+	}
+	else if (selector.charAt(0) == '#') {
+	    selType = SelectorType.IDENTIFIER;
+	    selector = selector.substring(1);
+	}
+	
+	List<UINode> matchingNodes = new ArrayList<UINode>();
+	CLI.find(node, selector, selType, matchingNodes);
+	
+	if (matchingNodes.size() == 0) {
+	    System.out.println("No nodes found");
+	}
+	else {
+	    System.out.println("Found " + matchingNodes.size() + " nodes:");
+	    for (UINode n : matchingNodes) {
+		System.out.println(n);
+		System.out.println();
+	    }
+	}
+    }
+    
+    public static void find(UINode node, String selector, SelectorType selType, List<UINode> matchingNodes) {
+	
+	// Does current node match, if so add to the list
+	switch(selType) {
+	case CLASS: {
+	    if (node.getUiClass() != null && node.getUiClass().equals(selector)) {
+		matchingNodes.add(node);
+	    }
+	    break;
+	}
+	case IDENTIFIER: {
+	    if (node.getIdentifier() != null && node.getIdentifier().equals(selector)) {
+		matchingNodes.add(node);
+	    }
+	    break;
+	}
+	case CLASSNAMES: {
+	    if (node.getClassNames().contains(selector)) {
+		matchingNodes.add(node);
+	    }
+	    break;
+	}
+	}
+	
+	// If the node has children continue traversing
+	if (node.getChildren().size() > 0) {
+	    for (UINode n : node.getChildren()) {
+		CLI.find(n, selector, selType, matchingNodes);
+	    }
+	}
+    }
 }
--- src/cantina/UINode.java
+++ src/cantina/UINode.java
@@ -7,91 +7,91 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeType;
 
 public class UINode {
-	private String name;
-	private String identifier;
-	private String uiClass;
-	private List<String> classnames = new ArrayList<String>();
-	private ArrayList<UINode> children = new ArrayList<UINode>();
-	private JsonNode node;
-
-	public UINode(String name, JsonNode node) {
-		this.node = node;
-		this.name = name;
-		
-		if (node.get("identifier") != null) {
-			this.identifier = node.get("identifier").asText();
-		}
-		if (node.get("class") != null) {
-			this.uiClass = node.get("class").asText();
-		}
-		
-		JsonNode n = node.get("classNames");
-		if (n != null && n.getNodeType() == JsonNodeType.ARRAY) {
-			for (JsonNode c : n) {
-				classnames.add(c.asText());
-			}
-		}
-		
-		JsonNode jsonContentView = node.get("contentView");
-		if (jsonContentView != null) {
-			this.children.add(new UINode("contentView", jsonContentView));
-		}
-
-		JsonNode jsonControl = node.get("control");
-		if (jsonControl != null) {
-			this.children.add(new UINode("control", jsonControl));
-		}
-
-		JsonNode jsonSubviewArray = node.get("subviews");
-		if (jsonSubviewArray != null && jsonSubviewArray.getNodeType() == JsonNodeType.ARRAY) {
-			for (JsonNode jsonSubview : jsonSubviewArray) {
-				UINode subview = new UINode("", jsonSubview);
-				this.children.add(subview);
-			}
-		}
+    private String name;
+    private String identifier;
+    private String uiClass;
+    private List<String> classnames = new ArrayList<String>();
+    private ArrayList<UINode> children = new ArrayList<UINode>();
+    private JsonNode node;
+    
+    public UINode(String name, JsonNode node) {
+	this.node = node;
+	this.name = name;
+	
+	if (node.get("identifier") != null) {
+	    this.identifier = node.get("identifier").asText();
+	}
+	if (node.get("class") != null) {
+	    this.uiClass = node.get("class").asText();
 	}
 	
-	public String getIdentifier() {
-		return identifier;
+	JsonNode n = node.get("classNames");
+	if (n != null && n.getNodeType() == JsonNodeType.ARRAY) {
+	    for (JsonNode c : n) {
+		classnames.add(c.asText());
+	    }
 	}
 	
-	public void setIdentifier(String identifier) {
-		this.identifier = identifier;
+	JsonNode jsonContentView = node.get("contentView");
+	if (jsonContentView != null) {
+	    this.children.add(new UINode("contentView", jsonContentView));
 	}
 	
-	public String getUiClass() {
-		return uiClass;
+	JsonNode jsonControl = node.get("control");
+	if (jsonControl != null) {
+	    this.children.add(new UINode("control", jsonControl));
 	}
 	
-	public void setUiClass(String uiClass) {
-		this.uiClass = uiClass;
+	JsonNode jsonSubviewArray = node.get("subviews");
+	if (jsonSubviewArray != null && jsonSubviewArray.getNodeType() == JsonNodeType.ARRAY) {
+	    for (JsonNode jsonSubview : jsonSubviewArray) {
+		UINode subview = new UINode("", jsonSubview);
+		this.children.add(subview);
+	    }
 	}
-	
-	public ArrayList<UINode> getChildren() {
-		return children;
-	}
-	
-	public void setChildren(ArrayList<UINode> children) {
-		this.children = children;
-	}	
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public List<String> getClassNames() {
-		return classnames;
-	}
-
-	public void setClassNames(List<String> classNames) {
-		this.classnames = classNames;
-	}
-
-	public String toString() {
-		return node.toString();
-	}
+    }
+    
+    public String getIdentifier() {
+	return identifier;
+    }
+    
+    public void setIdentifier(String identifier) {
+	this.identifier = identifier;
+    }
+    
+    public String getUiClass() {
+	return uiClass;
+    }
+    
+    public void setUiClass(String uiClass) {
+	this.uiClass = uiClass;
+    }
+    
+    public ArrayList<UINode> getChildren() {
+	return children;
+    }
+    
+    public void setChildren(ArrayList<UINode> children) {
+	this.children = children;
+    }	
+    
+    public String getName() {
+	return name;
+    }
+    
+    public void setName(String name) {
+	this.name = name;
+    }
+    
+    public List<String> getClassNames() {
+	return classnames;
+    }
+    
+    public void setClassNames(List<String> classNames) {
+	this.classnames = classNames;
+    }
+    
+    public String toString() {
+	return node.toString();
+    }
 }
